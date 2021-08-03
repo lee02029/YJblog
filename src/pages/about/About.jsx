d314ff2623479a38dc3eef70ac8e16a8956a0efe
minlee <kkang47140@gmail.com>
2021-08-04 04:41:16 +0900

    pages

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,10 +4,16 @@ export default function About() {
     return (
         <div className="about">      
             <div className="abouttop">
-                <img className="aboutimg"
+                <img className="aboutImg"
                  src="img/about.jpg"
                  alt=""
                 />             
+            </div>
+            <div className="aboutbottom">
+                <p>
+                I'm YoonJae Lee. I learn about React & Node JS 
+                and hope to be backend developer. I appreciate it.
+                </p>
             </div>
         </div>
     )
