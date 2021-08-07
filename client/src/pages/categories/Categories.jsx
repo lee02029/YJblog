1416c0c69e472d7a9683673be4d31178612f5618
minlee <kkang47140@gmail.com>
2021-08-08 00:55:49 +0900

    api 1.0

--- client/src/pages/categories/Categories.jsx
+++ client/src/pages/categories/Categories.jsx
@@ -3,8 +3,26 @@ import "./categories.css"
 export default function Categories() {
     return (
         <div className="categoies">
-            <d>All Posts React Node JS Computer Database Algoithm
-            </d>
+           <ul className="categoiesItem">
+               <li className="categoriesItem">
+                   All Posts
+               </li>
+               <li className="categoriesItem">
+                   React
+               </li>
+               <li className="categoriesItem">
+                   Node js
+               </li>
+               <li className="categoriesItem">
+                   Database
+               </li>
+               <li className="categoriesItem">
+                   Algorithm
+               </li>
+               <li className="categoriesItem">
+                   Project
+               </li>
+           </ul> 
         </div>
     );
 }
