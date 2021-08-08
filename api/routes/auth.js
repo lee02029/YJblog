e4a4a67805560c42d6f48016131d155f54da448b
minlee <kkang47140@gmail.com>
2021-08-08 20:20:33 +0900

    api 2.0

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,11 +4,13 @@ const bcrypt = require('bcrypt');
 
 //REGISTER
 router.post("/register",async (req,res)=>{
-    try {       
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password:req.body.password,
+            password:hashedPass,
         });
 
         const user = await newUser.save();
@@ -19,5 +21,19 @@ router.post("/register",async (req,res)=>{
 });
 
 //LOGIN
+router.post("/login", async (req,res)=>{
+    try {
+        const user = User.findOne({username: req.body.username})
+        !user && res.status(400).json("Wrong credentials")
+
+        const validated = await bcrypt.compare(req.body.password);
+        !validated && res.status(400).json(" Wrong credentails!");
+
+        const { password, ...others} = user._doc;
+        res.status(200).json(others); 
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
